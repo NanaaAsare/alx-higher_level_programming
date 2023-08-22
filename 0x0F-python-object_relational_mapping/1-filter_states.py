4d943a9eccc51e5c4b5731383146922d0fbb2afd
NanaaAsare <nananyarkoasare74@gmail.com>
2023-08-22 14:08:36 -0700

    python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+select_states module
+
+This script lists all states from the database hbtn_0e_0_usa
+where their name starts with `N`.
+The script takes in 3 arguments: mysql_username, mysql_password
+and mysql_database_name.
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+        db = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                                         passwd=sys.argv[2], db=sys.argv[3])
+            cursor = db.cursor()
+                cursor.execute('SELECT * FROM `states` WHERE name\
+                                                     LIKE "N%" ORDER BY `id` ASC')
+                    [print(state) for state in cursor.fetchall()]
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+select_states module
+
+This script lists all states from the database hbtn_0e_0_usa
+where name matches argument.
+The script takes in 4 arguments: mysql_username, mysql_password,
+mysql_database_name and state_name_searched.
+'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+            c = db.cursor()
+                c.execute("SELECT * FROM `states`")
+                    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
